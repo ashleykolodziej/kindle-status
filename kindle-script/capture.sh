9858f4fd25701bc075e0582cba40be508236cd16
Jim Reevior <jim@hirozed.me>
2021-10-10 20:37:11 -0400

    Kindle Script

---
+++ kindle-script/capture.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+rm /mnt/us/linkss/screensavers/done.png
+wget -O "/mnt/us/linkss/screensavers/done.png" "https://github.com/ashleykolodziej/kindle-status/blob/main/schedule.png"
+reboot
